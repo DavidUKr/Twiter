da07528f4b8e4d9e54c06780ec452e0bcff5bb9b
DavidUKr <DavidUKr@users.noreply.github.com>
2023-04-25 18:32:53 +0300

    poroject init

---
+++ src/test/java/app/Twiter/TwiterApplicationTests.java
@@ -0,0 +1,13 @@
+package app.Twiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
